047eb0f1d954d951c4828c074117021459cfef5f
Zach Frohardt <zfrohardt@gmail.com>
2021-05-06 17:27:41 -0700

    commit tree 0c961bdd1c04ac239ccef4def4847ce978814d5c

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,9 +4,21 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map(makeMovie)}
     </div>
   );
 };
+
+const makeMovie = (movie, key) => {
+  return (
+    <div key={key}>
+      {`${movie.title} | ${movie.time} minutes`}
+      <ul>
+        {movie.genres.map((genre, index) => <li key={index} >{genre}</li>)}
+      </ul>
+    </div>
+  );
+}
 
 export default Movies;
